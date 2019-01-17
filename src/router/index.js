6a15a5aa4110cae8b0bd9c783d944a56c9c8e6e1
yourname <2972946931@qq.com>
2019-01-17 19:13:54 +0800

    修改router数据

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ router.beforeEach((to, from, next) => {
     // let token = Cookies.get('loginToken');
 
     if (true) { // 是否拦截登录
-      if (true) { // 判断是否有sessionID，是否需要重新获取路由
+      if (false) { // 判断是否有sessionID，是否需要重新获取路由
         (async () => {
           await store.dispatch('getMenuList');
           // replace重新获取route对象
