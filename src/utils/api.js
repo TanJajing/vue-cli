ea244486a23fc1bd594d40e90fb112e1c700a045
yourname <2972946931@qq.com>
2019-01-18 19:59:54 +0800

    清除warnning

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,10 @@
 // import qs from 'qs';
 import request from './http/index.js';
 
-// const development = 'http://localhost:3618';
+const development = 'http://localhost:3618';
 // const path = '/new-mobile';
 const API = (url, method) => params => request(url, params, method || 'post');
 
 export default {
-  /* 公共 */
- };
+  loginApi: API(development + '/log')
+};
